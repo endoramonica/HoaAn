b887598fbfde8e875177d193f7ab58f845e686f2
nhu quynh <lydonganh44444@gmail.com>
2025-06-16 16:07:49 +0700

    Refactor: .Api + .GUI + .Shared

---
+++ startUp/SystemEcom/Lab-05/Lab-05.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab_05.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
